72d7ba714d7e0a4db1bd853cc488ed9af88e741d
Ahmed-Shahi <ahmedshahi038@gmail.com>
2024-03-24 15:39:00 +0500

    ok

---
+++ LAB 1/Task 1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ahmed Shahi");
+            Console.WriteLine("BSE-23F-110");
+            Console.WriteLine("\n");
+            Console.WriteLine("HELLO WORLD");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB 1/Task 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ahmed Shahi");
+            Console.WriteLine("BSE-23F-110");
+            Console.WriteLine("\n");
+            int age = 0;
+            Console.Write("Please Enter Your Age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"User Age: {age}");
+            Console.ReadKey();
+
+        }
+    }
+}
