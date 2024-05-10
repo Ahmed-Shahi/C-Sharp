ea9f8ecb65ed1d0917b9fa0d01dc6db88a768b26
Ahmed-Shahi <ahmedshahi038@gmail.com>
2024-05-11 02:15:45 +0500

    ok

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Add
+    {
+    public int c;
+
+          public void addition()
+        {
+            Console.WriteLine("First Number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second Number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+             c = a + b;
+            Console.WriteLine("The sum of {0} & {1} is : {2}", a, b, c);
+            Console.ReadKey();            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Add Sum = new Add();
+                Sum.addition();
+            }
+            while (i=c);
+            }
+    }
+}
