0143e5fcedb49e19b70b8346c5f8c6b0f9b46a8c
Ahmed-Shahi <ahmedshahi038@gmail.com>
2025-06-07 17:11:12 +0500

    ok

---
+++ CONNECTION/Connection/Connection/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+namespace Connection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connction = "server=localhost;user=root;password=ahmed038@;database=csharpdb";
+            MySqlConnection con = new MySqlConnection(connction);
+            string cmd = "insert into data value ('farooq','akram',24);";
+            MySqlCommand cm = new MySqlCommand(cmd,con);
+            try
+            {
+            con.Open();
+            cm.ExecuteNonQuery();
+                MessageBox.Show("Connected Successfully!!!");
+            }
+             catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+
+        }
+    }
+}
---
+++ Interface/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //public interface IExampleInterface
+    //{
+    //    void SomeMethod();
+
+    //}
+
+    //public class ExampleClass : IExampleInterface
+    //{
+    //    public void SomeMethod()
+    //    {
+    //        Console.WriteLine("Implementing Successfully!!!");
+    //        Console.ReadKey();
+    //    }
+
+    //}
+    public interface I1
+    {
+        void show1();
+    }
+    public interface I2
+    {
+        void show1();
+    }
+    
+
+    internal class Program : I1, I2
+    {
+        void I1.show1()
+        {
+            Console.WriteLine("INTERFACE 1...........");
+        }
+        void I2.show1()
+        {
+            Console.WriteLine("INTERFACE 2...........");
+        }
+        
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+             ((I1)obj).show1();
+             ((I2)obj).show1();
+
+            I1 I1obj = new Program();
+            I1obj.show1();
+
+            I2 I2obj = new Program();
+            I2obj.show1();
+
+        }
+    }
+}
